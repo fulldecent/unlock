040e847fca22b2332e4130175dccda7252ebaaec
Julien Genestoux <julien.genestoux@gmail.com>
2019-02-21 18:05:53 +0100

    Add semaphore.yml

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -6,11 +6,9 @@
 # When called with a second argument, will push the image under this tag too
 
 IMAGE_NAME=$1
-PUSH_IMAGE=$2
 REPO_ROOT=`dirname "$0"`/..
 DOCKERFILE=$REPO_ROOT/docker/$IMAGE_NAME.dockerfile
 ARGS=""
-DOCKER_TAG="build-$TRAVIS_BUILD_ID"
 
 if [ "$DOCKER_REPOSITORY" != "" ]; then
   IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:latest"
@@ -22,11 +20,4 @@ fi
 
 ARGS="$ARGS --cache-from $IMAGE_CACHE"
 
-docker build -t $IMAGE_NAME:$DOCKER_TAG -f $DOCKERFILE $ARGS $REPO_ROOT
-
-if [ "$PUSH_IMAGE" = "true" ]; then
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  IMAGE_TO_PUSH="$DOCKER_REPOSITORY/$IMAGE_NAME:$DOCKER_TAG"
-  docker tag "$IMAGE_NAME:$DOCKER_TAG" $IMAGE_TO_PUSH
-  docker push $IMAGE_TO_PUSH
-fi
+docker build -t $IMAGE_NAME -f $DOCKERFILE $ARGS $REPO_ROOT
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,19 +4,9 @@
 # It first pulls the images and then builds the cluster
 
 SERVICE=$1
+PUSHED_IMAGE=$2
 REPO_ROOT=`dirname "$0"`/..
 DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
 COMMAND="npm run ci"
-IMAGE_NAME="unlock"
-IMAGE_TAG="build-$TRAVIS_BUILD_ID"
-IMAGE_CACHE="$IMAGE_NAME:$IMAGE_TAG" # default
 
-if [ "$DOCKER_REPOSITORY" != "" ]; then
-  IMAGE_CACHE="$DOCKER_REPOSITORY/$IMAGE_NAME:$IMAGE_TAG"
-  echo "Pulling $IMAGE_CACHE to use as cache for $IMAGE_NAME"
-  docker pull $IMAGE_CACHE
-fi
-docker tag $IMAGE_CACHE $IMAGE_NAME
-
-docker-compose -f $DOCKER_COMPOSE_FILE build
 docker-compose -f $DOCKER_COMPOSE_FILE run -e CI=true $SERVICE $COMMAND
